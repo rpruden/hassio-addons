5dfaf8d86434b70ae86003c40bf64c47d95ec793
rpruden <Richard.prudencio@gmail.com>
2023-04-25 00:02:40 -0600

    Update run.sh

--- neolink/run.sh
+++ neolink/run.sh
@@ -14,17 +14,6 @@ echo "--- Neolink ---"
 if [ "${MODE}" == "dual" ]; then
     neolink rtsp --config /config/addons/neolink.toml & neolink mqtt --config /config/addons/neolink.toml & wait -n
 else
-    neolink ${MODE} --config /config/addons/neolink.toml &
-    {
-        while true; do
-            neolink image --config /config/addons/neolink.toml --file-path /config/snaps/livingroom.jpg livingroom;
-            sleep(10);
-        done;
-    } & {
-        while true; do
-            neolink image --config /config/addons/neolink.toml --file-path /config/snaps/masterbed.jpg masterbed;
-            sleep(10);
-        done;
-    } & wait -n
+    neolink ${MODE} --config /config/addons/neolink.toml & neolink image --config /config/addons/neolink.toml --file-path /config/snaps/livingroom.jpg livingroom & wait -n
 fi
 
