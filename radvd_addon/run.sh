0b2304983ffff5b77257434df321b7a41a24672e
rpruden <Richard.prudencio@gmail.com>
2025-08-13 12:54:53 -0600

    Update run.sh

--- radvd_addon/run.sh
+++ radvd_addon/run.sh
@@ -9,6 +9,12 @@ PREFIX=$(jq -r '.prefix' /data/options.json)
 INTERFACE=$(jq -r '.interface' /data/options.json)
 LOG_LEVEL=$(jq -r '.log_level' /data/options.json)
 
+# DHCPv6 configs from options.json
+DHCP_RANGE_START=$(jq -r '.dhcpv6_range_start // empty' /data/options.json)
+DHCP_RANGE_END=$(jq -r '.dhcpv6_range_end // empty' /data/options.json)
+# reserved leases: JSON array of objects with mac & ip properties
+LEASES=$(jq -c '.dhcpv6_leases // []' /data/options.json)
+
 # Translate string log level to numeric debug level for radvd
 case "$LOG_LEVEL" in
   none)
@@ -31,6 +37,7 @@ case "$LOG_LEVEL" in
     ;;
 esac
 
+# Generate radvd.conf (SLAAC advertisements)
 cat <<EOF > /etc/radvd.conf
 interface ${INTERFACE} {
   AdvSendAdvert on;
@@ -43,4 +50,40 @@ interface ${INTERFACE} {
 };
 EOF
 
-exec radvd -n $RADVD_ARGS -C /etc/radvd.conf
+# Generate dhcpd6.conf (DHCPv6 server config)
+# This will only create a pool if DHCP_RANGE_START and DHCP_RANGE_END are provided
+cat <<EOF > /etc/dhcpd6.conf
+default-lease-time 600;
+max-lease-time 7200;
+log-facility local7;
+
+subnet6 ${PREFIX} {
+EOF
+
+if [ -n "$DHCP_RANGE_START" ] && [ -n "$DHCP_RANGE_END" ]; then
+cat <<EOF >> /etc/dhcpd6.conf
+  range6 ${DHCP_RANGE_START} ${DHCP_RANGE_END};
+EOF
+fi
+
+# Add static leases if any
+for lease in $(echo "$LEASES" | jq -c '.[]'); do
+  MAC=$(echo "$lease" | jq -r '.mac')
+  IP=$(echo "$lease" | jq -r '.ip')
+  cat <<EOF >> /etc/dhcpd6.conf
+  host reserved-${MAC//:/} {
+    hardware ethernet $MAC;
+    fixed-address6 $IP;
+  };
+EOF
+done
+
+cat <<EOF >> /etc/dhcpd6.conf
+}
+EOF
+
+# Start radvd in foreground with log level
+radvd -n $RADVD_ARGS -C /etc/radvd.conf &
+
+# Start DHCPv6 server for your interface
+dhcpd -6 -cf /etc/dhcpd6.conf $INTERFACE
